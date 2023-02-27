905cc6421d0d20e10c68cc4c306dac966e949370
Luan Braz <luan_silvacpv@hotmail.com>
2023-02-27 08:51:27 -0300

    classe model e mock com questões

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,15 @@
+import { Frase } from "../shared/frase.model";
+
+const FRASES: Frase[] = [
+  { fraseEng: "I'm running a little late.", frasePtBr: "Eu estou um pouco atrasado." },
+  { fraseEng: "Are you up to anything tonight? ", frasePtBr: "Você vai fazer alguma coisa hoje à noite?" },
+  { fraseEng: "You've got to be kidding me! ", frasePtBr: "Você só pode estar brincando comigo!" },
+  { fraseEng: "It's not worth it! ", frasePtBr: "Não vale a pena." },
+  { fraseEng: "Pull yourself together.", frasePtBr: "Se controle, se acalme." },
+  { fraseEng: "You sold me!", frasePtBr: "Você me convenceu!" },
+  { fraseEng: "Take care!", frasePtBr: "Se cuide!" },
+  { fraseEng: "Do you want to order something to eat?", frasePtBr: "Você quer pedir alguma coisa para comer?" },
+  { fraseEng: "Don't take it to heart", frasePtBr: "Não leve para o lado pessoal." },
+  { fraseEng: "I like to learn", frasePtBr: "Eu gosto de aprender" },
+  { fraseEng: "I watch tv", frasePtBr: "Eu assisto tv" }
+]
