0c1b9cd7d82b05e95495f4eab124bea92be41d08
Luan Braz <luan_silvacpv@hotmail.com>
2023-03-04 11:46:08 -0300

    event binding

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -5,20 +5,21 @@ import { FRASES } from './frases-mock';
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
-  styleUrls: ['./painel.component.css']
+  styleUrls: ['./painel.component.css'],
 })
 export class PainelComponent implements OnInit {
-
   public frases: Frase[] = FRASES;
-  public instrucao: string = 'Traduza a frase'
+  public instrucao: string = 'Traduza a frase';
+  resposta!: string;
 
   constructor() {
     console.log(this.frases);
-
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    console.log(this.resposta);
   }
-
-
 }
